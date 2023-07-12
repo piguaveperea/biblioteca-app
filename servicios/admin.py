81c8e26ec78a6ced6d0e05b2716360f045ab3d5a
Victor <piguavepere@gmail.com>
2023-07-11 20:22:16 -0500

    insert services

--- servicios/admin.py
+++ servicios/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Servicio, Registro_Servicio
 # Register your models here.
+
+
+admin.site.register(Servicio)
+admin.site.register(Registro_Servicio)
